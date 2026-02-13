039a2fee62e79188ca081965664d1f34507d4b98
AKanshPatel <akansh_avkalan@gmail.com>
2026-02-13 14:47:23 +0530

    correct conditional

--- 5_chains/4_conditional_chain.py
+++ 5_chains/4_conditional_chain.py
@@ -1,35 +1,55 @@
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain_core.output_parsers import PydanticOutputParser 
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import Literal
 
-# API key 
+# API
 load_dotenv()
 
-# Model:
+# Model: 
 chat_model = ChatGroq(
     model = "llama-3.1-8b-instant"
-)  
-
-# Base class - Pydantic class
-class Feedback(BaseModel): 
-    sentiment : Literal["positive", "negative"] = Field(description = "Give me the sentiment of the given feedback" )
-
-# Parser - Setup parser + inject the instructions 
-parser = PydanticOutputParser(pydantic_object = Feedback)
-
-# prompt
-prompt1 = PromptTemplate(
-    template = "Analyze the following feedback and classify it strictly as positive or negative.\n {format_instructions} \n Feedback {feedback}",
-    input_variables = ["feedback"], 
-    partial_variables = {"format_instructions": parser.get_format_instructions()} 
 )
 
-# Chain 
-sentiment_chain = prompt1 | chat_model | parser
+parser = StrOutputParser()
 
-response = sentiment_chain.invoke([{'feedback': "Terrible product Cons- ultra wide camera is missing, LCD display is there, additional noise cancellation microphone not provided"}]).sentiment
+class Feedback(BaseModel):
 
-print(response)
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | chat_model | parser2
+
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | chat_model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | chat_model | parser),
+    RunnableLambda(lambda x: "could not find sentiment")
+)
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
+
+chain.get_graph().print_ascii()
